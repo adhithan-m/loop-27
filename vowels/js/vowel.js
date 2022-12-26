c37cffafa2c808af391a31811acd1c9910b4abc8
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 20:12:03 +0530

    vowels.

---
+++ vowels/js/vowel.js
@@ -0,0 +1,14 @@
+function vovel(b)
+{
+a=prompt("enter the word")
+//alert(a.length)
+c=0
+for(i=0;i<a.length;i++){
+	if(a[i]!=='a' &&  a[i]!=='e' && a[i]!=='i'  && a[i]!=='o'  && a[i]!=='u')
+	{
+		document.write(a[i]);
+		c+=1
+}}	99
+/*return c
+}
+
